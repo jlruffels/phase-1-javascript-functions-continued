c9a0ea75b46a3b61914fb5d0a9622bf14c056dca
Jessica RRuffels <jlruffels@yahoo.com>
2023-03-15 10:59:46 -0400

    functionscont

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return (`This Saturday, I want to ${activity}!`);
+}
+saturdayFun();
+saturdayFun("bathe my dog")
+saturdayFun();
+
+
+function mondayWork(activity = "go to the office") {
+    return (`This Monday, I will ${activity}.`)
+}
+
+function wrapAdjective(adj1 = "*") {
+    return function(adjective = "special"){
+        return `You are ${adj1}${adjective}${adj1}!`
+    }
+}
+console.log(wrapAdjective())
